5dd053686bea6a8a22495f8a5b0280e63395f4c5
Matt <matta628@gmail.com>
2022-11-19 22:03:18 -0500

    Add operate fn that calls basic math fns

--- calculator.js
+++ calculator.js
@@ -11,3 +11,24 @@ function divide(op1, op2){
     return +op1 / +op2;
 }
 
+function operate(op, op1, op2){
+    let result = 0;
+    switch (op){
+        case '+':
+            result = add(op1,op2);
+            break;
+        case '-':
+            result = subtract(op1,op2);
+            break;
+        case '*':
+            result = multiply(op1,op2);
+            break;
+        case '/':
+            result = divide(op1,op2);
+            break;
+        default:
+            result = "wot tha fuck you doing mate";
+    }
+    return result;
+}
+
