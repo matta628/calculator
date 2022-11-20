2502f99d228d120bc10b532ddbb84a36237edf35
Matt <matta628@gmail.com>
2022-11-19 23:32:25 -0500

    Add display update functions for digits and clear

--- calculator.js
+++ calculator.js
@@ -32,3 +32,30 @@ function operate(op, op1, op2){
     return result;
 }
 
+const display = document.querySelector('.display');
+function addDigits(button){
+    if (display.innerText === '0'){
+        display.innerText = button.innerText;
+    }
+    else if (+display.innerText === +display.innerText){
+        //if it's a number.. append
+        display.innerText += button.innerText;
+    }
+    else{
+        display.innerText = '0';
+    }
+}
+const digitButtons = document.querySelectorAll('button.digit');
+digitButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        addDigits(button);
+    });
+});
+
+const clearButton = document.querySelector('#clear');
+function clearDisplay(){
+    display.innerText = '0';
+}
+clearButton.addEventListener('click', () => {
+    clearDisplay();
+});
