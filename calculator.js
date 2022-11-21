03cf69ef2d6e945f8794f0d3bcaf31a215a5f339
Matt <matta628@gmail.com>
2022-11-21 18:00:29 -0500

    Add evaluative capabilities

--- calculator.js
+++ calculator.js
@@ -20,7 +20,7 @@ function operate(op, op1, op2){
         case '-':
             result = subtract(op1,op2);
             break;
-        case '*':
+        case 'x':
             result = multiply(op1,op2);
             break;
         case '/':
@@ -32,15 +32,76 @@ function operate(op, op1, op2){
     return result;
 }
 
+let startNewNumber = true;
+let lhs = null;
+let rhs = null;
+let operator = null;
+
 const display = document.querySelector('.display');
+
+function evaluate(button){
+    startNewNumber = true;
+
+    if (lhs === null){ //need left side
+        lhs = +display.innerText;
+    }
+    else{ //don't need left side
+        if (rhs === null){ //need right side
+            rhs = +display.innerText;
+        }
+    }
+    
+    if (operator !== null){ //save result so far
+        lhs = operate(operator, lhs, rhs);
+        rhs = null;
+    }
+    display.innerText = lhs;
+    operator = button.innerText;
+    console.log(`lhs=${lhs}\top=${operator}\trhs=${rhs}`);
+}
+
+function equals(){
+    if (lhs === null) return;
+    if (rhs === null){
+        if (display.innerText !== ''){
+            rhs = +display.innerText;
+        }
+    }
+    lhs = operate(operator, lhs, rhs);
+    display.innerText = lhs;
+    console.log(`lhs=${lhs}\top=${operator}\trhs=${rhs}`);
+}
+
 function addDigits(button){
-    if (display.innerText === '0'){
+    if (startNewNumber){
         display.innerText = button.innerText;
+        startNewNumber = false;
     }
     else {
         display.innerText += button.innerText;
     }
 }
+
+function clearDisplay(){
+    display.innerText = '';
+    lhs = null;
+    rhs = null;
+    operator = null;
+    startNewNumber = true;
+}
+
+const operatorButtons = document.querySelectorAll('button.operator');
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        evaluate(button);
+    })
+});
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', () => {
+    equals();
+});
+
 const digitButtons = document.querySelectorAll('button.digit');
 digitButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -49,9 +110,11 @@ digitButtons.forEach(button => {
 });
 
 const clearButton = document.querySelector('#clear');
-function clearDisplay(){
-    display.innerText = '0';
-}
 clearButton.addEventListener('click', () => {
     clearDisplay();
 });
+
+
+
+
+
